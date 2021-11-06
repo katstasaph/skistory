209493d49f6277d59541380d5f6cf011d0158abb
katstasaph <katherinestasaph@gmail.com>
2021-11-06 00:22:05 -0400

    clear out some boilerplate that I should have cleared out from the start

--- skifunctions.py
+++ skifunctions.py
@@ -1,37 +1,4 @@
-#
-# Copyright (C) 2010-2015 Nektra S.A., Buenos Aires, Argentina.
-# All rights reserved. Contact: http://www.nektra.com
-#
-#
-# This file is part of Deviare
-#
-#
-# Commercial License Usage
-# ------------------------
-# Licensees holding valid commercial Deviare licenses may use this file
-# in accordance with the commercial license agreement provided with the
-# Software or, alternatively, in accordance with the terms contained in
-# a written agreement between you and Nektra.  For licensing terms and
-# conditions see http://www.nektra.com/licensing/. Use the contact form
-# at http://www.nektra.com/contact/ for further information.
-#
-#
-# GNU General Public License Usage
-# --------------------------------
-# Alternatively, this file may be used under the terms of the GNU General
-# Public License version 3.0 as published by the Free Software Foundation
-# and appearing in the file LICENSE.GPL included in the packaging of this
-# file.  Please visit http://www.gnu.org/copyleft/gpl.html and review the
-# information to ensure the GNU General Public License version 3.0
-# requirements will be met.
-#
-#
-
-# Auxiliar Functions =====================================================================
-
-from subprocess import *
-import os, sys
-
+import sys
 
 def GetPIDByProcessName(aProcessName):
     for proc in psutil.process_iter():
@@ -49,11 +16,9 @@ def openSkiFree(spyManager):
 
 
 def HookFunctionForProcess(spyManager, functionModuleAndName, skifreePID):
-    print("Hooking function " + functionModuleAndName + " for SkiFree...")
     hook = spyManager.CreateHook(functionModuleAndName, 0)
     hook.Attach(skifreePID, True)
     hook.Hook(True)
-    print("Notepad successfully hooked")
     return hook
 
 
